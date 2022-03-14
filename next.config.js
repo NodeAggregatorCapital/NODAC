4f636c742d6bc3ea78c0baaa37dfe87554a9da9e
Dylan Ramsey <contact@dylanramsey.com>
2022-03-13 17:52:27 -0700

    .

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   env: {
+    //Titles, Labels and Taglines
     appTitle: "Flip It!", //Title of the app - header.js
     tagline: "Try your luck!!", //Tagline displayed below coin image - dashboard.js
     heads: "You're staking HEADS!!!", //Tagline when user clicks Head Image - stakePlay.js
     tails: "You're staking TAILS!!!", //Tagline when user clicks Tail Image - stayPlay.js
     amountTxt: "How much are you willing to stake?", //Text above textfield - stakePlay.js
+    streaks: "Got 1!, Scores a Double, You're Killing It!, You're on Fire!", //List of STREAK NAMES - stakeyPlay.js
 
     //Image File Names
     coinImg: "coin_spin_bg.gif", //Name of main coin image - dashboard.js
     headsImg: "coin_heads.svg", //Name of the coin image for heads - stakePlay.js
     tailsImg: "coin_tails.svg", //Name of the coin image for tails - stakePlay.js
     fireImg: "streakfire.gif", //Name of the fire image for streaks = stakePlay.js
-
-    streaks: "Got 1!, Scores a Double, You're Killing It!, You're on Fire!", //List of STREAK NAMES - stakeyPlay.js
 
     //Result Lows Limit
     limitResults: 10, //Limits the number of ALL results, does not limit the number of filtered results - dashboard.js
